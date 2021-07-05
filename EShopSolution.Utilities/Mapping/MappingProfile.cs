614e439d9cc037e53c00cba2a2dee44e663648f0
lehuy-dev <lehuy.developer@gmail.com>
2021-07-05 17:39:33 +0700

    build utilities

---
+++ EShopSolution.Utilities/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EShopSolution.Data.Entities;
+using EShopSolution.ViewModels.Catalog.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopSolution.Utilities.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap<Product, ProductVm>();
+        }
+    }
+}
