edd99425fff4d22a60cc7f2f6ceee41c0c6e9211
lehuy-dev <lehuy.developer@gmail.com>
2021-07-13 14:01:34 +0700

    create user service

--- EShopSolution.Application/System/Users/UserService.cs
+++ EShopSolution.Application/System/Users/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using EShopSolution.Data.Entities;
+using EShopSolution.Utilities.Tokens;
 using EShopSolution.ViewModels.Common;
 using EShopSolution.ViewModels.System.Users;
 using Microsoft.AspNetCore.Identity;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,20 +19,42 @@ namespace EShopSolution.Application.System.Users
         private readonly SignInManager<AppUser> _signInManager;
         private readonly RoleManager<AppRole> _roleManager;
         private readonly IConfiguration _config;
+        private readonly JwtAuthentication _jwtAuthentication;
         public UserService(UserManager<AppUser> userManager,
             SignInManager<AppUser> signInManager,
             RoleManager<AppRole> roleManager,
-            IConfiguration config)
+            IConfiguration config,
+            JwtAuthentication jwtAuthentication)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _roleManager = roleManager;
             _config = config;
+            _jwtAuthentication = jwtAuthentication;
         }
         
-        public Task<ApiResult<string>> Authencate(LoginRequest request)
+        public async Task<ApiResult<string>> Authencate(LoginRequest request)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user == null)
+            {
+                return new ApiErrorResult<string>("Account khong ton tai");
+            }
+            var result = await _signInManager.PasswordSignInAsync(user.UserName, request.Password, request.RememberMe,true);
+            if (result.Succeeded)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.Role, string.Join(";",roles)),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.GivenName, user.FirstName)
+                };
+
+                return new ApiSuccessResult<string>(_jwtAuthentication.BuildToken(claims));
+            }
+            return new ApiErrorResult<string>();
         }
 
         public Task<ApiResult<bool>> Delete(Guid id)
@@ -48,9 +72,32 @@ namespace EShopSolution.Application.System.Users
             throw new NotImplementedException();
         }
 
-        public Task<ApiResult<bool>> Register(RegisterRequest request)
+        public async Task<ApiResult<bool>> Register(RegisterRequest request)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if(user != null)
+            {
+                return new ApiErrorResult<bool>("Tai khoan da ton tai!");
+            }
+            if(await _userManager.FindByEmailAsync(request.Email)!=null)
+            {
+                return new ApiErrorResult<bool>("Email da ton tai!");
+            }
+            user = new AppUser()
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Dob = request.Dob,
+                PhoneNumber = request.PhoneNumber
+            };
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (result.Succeeded)
+            {
+                return new ApiSuccessResult<bool>(true);
+            }
+            return new ApiErrorResult<bool>("Dang ky khong thanh cong");
         }
 
         public Task<ApiResult<bool>> RoleAssign(Guid id, RoleAssignRequest request)
